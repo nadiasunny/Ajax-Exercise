23840b469738e6ccc2b7d29f9743ef0e46e6102d
Nadia Hina <nadianadia062@gmail.com>
2023-12-20 15:48:16 -0800

    As much as Agne & I could get done

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -4,6 +4,16 @@
 
 function showFortune(evt) {
   // TODO: get the fortune and show it in the #fortune-text div
+  
+  //fetch waits for response from /fortune, so it returns promise
+  fetch('/fortune')
+    //return text, response.text parses text and returns another promise
+    .then((response)=>response.text())
+    //once we have that parsed text we can 
+    .then((serverData) => {
+      //set our div text to that text
+      document.querySelector('#fortune-text').innerText = serverData;
+    });
 }
 
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
@@ -12,11 +22,19 @@ document.querySelector('#get-fortune-button').addEventListener('click', showFort
 
 function showWeather(evt) {
   evt.preventDefault();
-
-  const url = '/weather.json';
+  
+  
   const zipcode = document.querySelector('#zipcode-field').value;
+  url = `/weather.json?zipcode=${zipcode}`;
 
   // TODO: request weather with that URL and show the forecast in #weather-info
+  fetch(url)
+    .then((response)=>response.json())
+    .then((weatherData) => { 
+      document.querySelector('#weather-info').innerText = weatherData['forecast'];
+    });
+   
+  
 }
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
@@ -25,7 +43,19 @@ document.querySelector('#weather-form').addEventListener('submit', showWeather);
 
 function orderMelons(evt) {
   evt.preventDefault();
+  
+  let status = document.getElementById('order-status');
+  let mesDet = {melon, qty}
+  // url = '/order-melons.json';
+  
+  fetch('/order-melons.json', {data:mesDet})
+    .then((response) => response.json())
+    .then((data) => {
+      status.innerHTML = data['msg'];
 
+      //status.innerHTML = data.message;
+    });
+    
   // TODO: show the result message after your form
   // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
 }
