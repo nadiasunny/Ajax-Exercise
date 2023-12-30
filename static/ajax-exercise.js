d4f5bf2ea339d4c593455ffc8ed8aab2566210c2
Nadia Hina <nadianadia062@gmail.com>
2023-12-29 17:57:33 -0800

    Lab & doggos complete

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -12,7 +12,7 @@ function showFortune(evt) {
     //once we have that parsed text we can 
     .then((serverData) => {
       //set our div text to that text
-      document.querySelector('#fortune-text').innerText = serverData;
+      document.querySelector('#fortune-text').innerHTML = serverData;
     });
 }
 
@@ -23,9 +23,8 @@ document.querySelector('#get-fortune-button').addEventListener('click', showFort
 function showWeather(evt) {
   evt.preventDefault();
   
-  
   const zipcode = document.querySelector('#zipcode-field').value;
-  url = `/weather.json?zipcode=${zipcode}`;
+  let url = `/weather.json?zipcode=${zipcode}`;
 
   // TODO: request weather with that URL and show the forecast in #weather-info
   fetch(url)
@@ -34,7 +33,6 @@ function showWeather(evt) {
       document.querySelector('#weather-info').innerText = weatherData['forecast'];
     });
    
-  
 }
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
@@ -43,15 +41,28 @@ document.querySelector('#weather-form').addEventListener('submit', showWeather);
 
 function orderMelons(evt) {
   evt.preventDefault();
-  
   let status = document.getElementById('order-status');
-  let mesDet = {melon, qty}
+  let qty = document.getElementById('qty-field').value;
+  let melon = document.querySelector('#melon-type-field').value;
+
+  let mesDet = {'melon_type': melon, 'qty': qty};
   // url = '/order-melons.json';
   
-  fetch('/order-melons.json', {data:mesDet})
+  fetch('/order-melons.json', {
+    method: 'POST',
+    body: JSON.stringify(mesDet),
+    headers: {'Content-Type': 'application/json',
+    },
+  })
     .then((response) => response.json())
     .then((data) => {
-      status.innerHTML = data['msg'];
+      if(data.code !== 'ERROR'){
+        status.classList.remove('order-error');
+        status.innerHTML = `${data.msg}`;} 
+        else {
+        status.innerHTML = `${data.msg}`;
+        status.classList.add('order-error');
+      };
 
       //status.innerHTML = data.message;
     });
@@ -60,3 +71,14 @@ function orderMelons(evt) {
   // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
 }
 document.querySelector('#order-form').addEventListener('submit', orderMelons);
+
+
+function doggos(evt){
+  fetch('https://dog.ceo/api/breeds/image/random')
+  .then((response) => response.json())
+  .then((data) => {
+    document.querySelector('#dog-image').insertAdjacentHTML('beforeend', `<img src='${data.message}'/>`);
+  })
+}
+
+document.querySelector('#get-dog-image').addEventListener('click', doggos);
